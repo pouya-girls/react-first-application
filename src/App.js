8f2b26acb087aff9ec12326ab52af405d7288067
Mohammad Nourinik <mohammad@inpin.co>
2018-04-05 18:37:46 +0430

    Make app.js to say hello and good bye :)

--- src/App.js
+++ src/App.js
@@ -3,18 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props)
+
+        this._btnClickMeClicked = this._btnClickMeClicked.bind(this)
+
+        this.state = {
+            text: "Hello"
+        }
+    }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+          <h1 className={"textCenter"}>{this.state.text}</h1>
+          <button onClick={this._btnClickMeClicked}>Click me!</button>
       </div>
     );
+  }
+
+  _btnClickMeClicked() {
+      this.setState({
+          text: "Bye!"
+      });
   }
 }
 
